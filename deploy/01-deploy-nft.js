2f00bd313ef796f333470e9f757cee3a50ce827f
makischristou <makis_christou@protonmail.com>
2023-07-27 11:20:25 +0300

    Deploy using Web3 instead of Etheres

--- deploy/01-deploy-nft.js
+++ deploy/01-deploy-nft.js
@@ -11,18 +11,18 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log("----------------------------------------------------");
 
     // Deploy ERC6551Registry
-    const ERC6551Registry = await ethers.getContractFactory("ERC6551Registry");
-    const ERC6551registry = await ERC6551Registry.deploy();
+    const ERC6551Registry = artifacts.require("ERC6551Registry");
+    const ERC6551registry = await ERC6551Registry.new();
 
     // Deploy ERC6551AccountImplementation
-    const SimpleERC6551Account = await ethers.getContractFactory("SimpleERC6551Account");
-    const SimpleERC6551account = await SimpleERC6551Account.deploy();
+    const SimpleERC6551Account = artifacts.require("SimpleERC6551Account");
+    const SimpleERC6551account = await SimpleERC6551Account.new();
 
     log("ERC6551Registry: ", ERC6551registry.address);
     log("SimpleERC6551Account: ", SimpleERC6551account.address);
 
     // Deployed address, currently all the same for all chains https://docs.tokenbound.org/contracts/deployments
-    const arguments = ["0x02101dfB77FDE026414827Fdc604ddAF224F0921", "0x2d25602551487c3f3354dd80d76d54383a243358"];
+    const arguments = [ERC6551registry.address, SimpleERC6551account.address];
     const nft = await deploy("ManagersTBA", {
         from: deployer,
         args: arguments,
