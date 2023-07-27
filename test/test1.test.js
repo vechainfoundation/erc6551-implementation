ed12b03ca452b064277510acd20ebc78a0b0df60
makischristou <makis_christou@protonmail.com>
2023-07-27 12:32:08 +0300

    Check if address given is correct

--- test/test1.test.js
+++ test/test1.test.js
@@ -1,18 +1,16 @@
 const ManagersTBA = artifacts.require('ManagersTBA');
 const { expect } = require('chai');
+const ADDRESS = "0xe5b22a7ea2921867D00C7E2853f084bF1a420bb8"
 
 contract('ManagersTBA', function (accounts) {
   beforeEach(async function () {
-    this.ManagersTBA = await new ManagersTBA("0xe5b22a7ea2921867D00C7E2853f084bF1a420bb8");
+    
+    this.ManagersTBA = await new ManagersTBA(ADDRESS);
   });
 
-  it('default value is 0', async function () {
+  it('Address is correct', async function () {
       var ret = await this.ManagersTBA;
-      
-      // 0x575081D1590bA48C72d5c92188Af12BB6359FcC1
-      console.log("ManagersTBA address: ", ret.address)
-
-      expect(true);
+      expect(ret.address).to.equal(ADDRESS);
     });
 
 });
