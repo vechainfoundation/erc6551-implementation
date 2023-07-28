378bb83cd60631bf4f34d07a05742c44c454eaa0
makischristou <makis_christou@protonmail.com>
2023-07-28 11:53:00 +0300

    Refactor NFT Tests and Update Minting and Transfer Functions

--- test/ERC6551.test.js
+++ test/ERC6551.test.js
@@ -1,28 +1,49 @@
 const ManagersTBA = artifacts.require('ManagersTBA');
 const MyToken = artifacts.require('MyToken');
 const { expect } = require('chai');
-const ADDRESS = "0x6579A6F2c20f0FA2e011d19093f7665b41979446"
+const NFT_ADDRESS = "0x814DB7e571DE4A5F38B1d596051A96bE5302bDEc"
+
 
 contract('ManagersTBA', function (accounts) {
+  const FIRST_OWNER = accounts[0]
+  const NEW_OWNER = accounts[1]
+
   beforeEach(async function () {
-    this.ManagersTBA = await new ManagersTBA(ADDRESS);
+    this.nft = await new ManagersTBA(NFT_ADDRESS);
   });
 
   it('Address is correct', async function () {
-      var nft = await this.ManagersTBA;
-      expect(nft.address).to.equal(ADDRESS);
+      expect(this.nft.address).to.equal(NFT_ADDRESS);
     });
 
-    it('Mint to random address', async function () {
-        var nft = await this.ManagersTBA;
-        var mintResult = await nft.mint(1, "0x1D94FAA58597C0417207681a512Df24Bb7702acb", {value: 1000000000000000000});
+    describe("Minting", async function(){
 
+      it('Mint to random address', async function () {
+        var mintedSoFar = (await this.nft.totalSupply()).toNumber();
+        var mintResult = await this.nft.mint(mintedSoFar + 1, FIRST_OWNER, {value: 1000000000000000000});
+        
         expect(mintResult);
     });
 
-    it("Newly minted NFT's owner is correct", async function(){
-      var ownerAddress = await nft.ownerOf(1);
-      expect(ownerAddress).to.equal("0x1D94FAA58597C0417207681a512Df24Bb7702acb");
-    });
+      it("Newly minted NFT's owner is correct", async function(){
+        var mintedSoFar = (await this.nft.totalSupply()).toNumber();
+        var ownerAddress = await this.nft.ownerOf(mintedSoFar);
 
+        expect(ownerAddress).to.equal(FIRST_OWNER);
+      });
+
+      it("Transfer NFT to new ownwer", async function(){
+        var mintedSoFar = (await this.nft.totalSupply()).toNumber();
+        var transferResult = await this.nft.transferFrom(FIRST_OWNER, NEW_OWNER, mintedSoFar)      
+
+        expect(transferResult);
+      });
+
+      it("NFT has been sent to new owner", async function(){
+        var mintedSoFar = (await this.nft.totalSupply()).toNumber();
+        var ownerAddress = await this.nft.ownerOf(mintedSoFar);
+        expect(ownerAddress).to.equal(NEW_OWNER);
+      });
+
+    });
 });
