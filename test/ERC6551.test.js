3d1cfee6a9c6a04d0d7f1e368531f908649908ff
makischristou <makis_christou@protonmail.com>
2023-07-31 14:50:46 +0300

    Send/Receive VTHO

--- test/ERC6551.test.js
+++ test/ERC6551.test.js
@@ -2,7 +2,7 @@ const ManagersTBA = artifacts.require('ManagersTBA');
 const SimpleERC6551Account = artifacts.require("SimpleERC6551Account");
 const MyToken = artifacts.require('MyToken');
 const { expect } = require('chai');
-const NFT_ADDRESS = "0x37d2f350cA339D766e927b396c9F48336e427c31"
+const NFT_ADDRESS = "0x510CfA4a2bF2E9b1De1D1B231C86790A45F9bA33"
 
 
 contract('ManagersTBA', function (accounts) {
@@ -54,32 +54,45 @@ contract('ManagersTBA', function (accounts) {
 
       it('Send ERC20', async function () {
 
+        var mintedSoFar = (await this.nft.totalSupply()).toNumber();
+        var ownerOfLastMintedNFT = await this.nft.showTBA(mintedSoFar);
         const VTHO = await new MyToken("0x0000000000000000000000000000456E65726779");
-        var nftBalance = await VTHO.balanceOf(this.nft.address);
-        console.log("Before NFT VTHO Balance: ", nftBalance.toString());
-        console.log("Sending VTHO to NFT");
-        await VTHO.transfer(this.nft.address, 1000000000000000)
-        var nftBalance = await VTHO.balanceOf(this.nft.address);
-        console.log("After NFT VTHO Balance: ", nftBalance.toString());
+        var accountBalance = await VTHO.balanceOf(ownerOfLastMintedNFT);
+        console.log("Before NFT VTHO Balance: ", accountBalance.toString());
+        console.log("Sending VTHO to Account");
+        await VTHO.transfer(ownerOfLastMintedNFT, 1000000000000000, {from: FIRST_OWNER})
+        var accountBalance = await VTHO.balanceOf(ownerOfLastMintedNFT);
+        console.log("After NFT VTHO Balance: ", accountBalance.toString());
 
-        expect(true);
+        const myTokenABI = VTHO.abi;
+
+        const functionSignature = 'transfer(address,uint256)';
+        const addressToSendTo = '0xb1690c08e213a35ed9bab7b318de14420fb57d8c';
+        const amountToSend = ethers.utils.parseUnits("1", 1);
+
+        var encodedFunctionCall = ethers.utils.defaultAbiCoder.encode(
+          ['string', 'address', 'uint256'],
+          [functionSignature, addressToSendTo, amountToSend]
+        );
+        
+        const contractInterface = new ethers.utils.Interface(myTokenABI);
+        var encodedFunctionCall = contractInterface.encodeFunctionData(functionSignature, [addressToSendTo, amountToSend]);
+
+        // console.log(encodedFunctionCall);
+
+
+        // Run executeCall to send VET out of Token bound account
+        var mintedSoFar = (await this.nft.totalSupply()).toNumber();
+        var ownerOfLastMintedNFT = await this.nft.showTBA(mintedSoFar);
+
+        // console.log("Owner of last minted NFT: ", ownerOfLastMintedNFT);
+        var accountContract = await new SimpleERC6551Account(ownerOfLastMintedNFT);
+        var tx = await accountContract.executeCall("0x0000000000000000000000000000456E65726779", 0, encodedFunctionCall, {from: NEW_OWNER});
+
+        expect(tx);
     });
 
-    it("Receive ERC20", async function() {
-
-      console.log("Owner: ", await this.nft.owner());
-
-      // var accountAddress = await this.nft.
-
-      // console.log("Account of NFT: ", accountAddress);
-
-      // var simpleERC6551Account = new SimpleERC6551Account(accountAddress);
-      // console.log(simpleERC6551Account)
-
-      expect(true)
-    });
-
-    it("Withdraw previously sent VET", async function () {
+    it("Withdraw previously sent VET from NFT", async function () {
       var result = await this.nft.withdraw(); // withdraws all balance
       expect(result);
     });
@@ -97,7 +110,7 @@ contract('ManagersTBA', function (accounts) {
       var tx1 = await accountContract.initialize({value: 100000000000000000000});
 
       // Run executeCall to send VET out of Token bound account
-      var tx2 = await accountContract.executeCall("0x25Df024637d4e56c1aE9563987Bf3e92C9f534c0", 1000000000000000000, [], {from: NEW_OWNER});
+      var tx2 = await accountContract.executeCall("0x25Df024637d4e56c1aE9563987Bf3e92C9f534c0", 1, [], {from: NEW_OWNER});
 
       expect(true)
     });
